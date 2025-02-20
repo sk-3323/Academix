8d94818b89e67f9996c99c3f99be2cc2eacc7b52
Rishi Gaiwala <rishigaiwala@gmail.com>
2025-02-20 11:07:50 +0530

    student dashboard created

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,4 @@
-export const NODE_APP_URL = "http://localhost:8080";
+export const NODE_APP_URL = "http://localhost:3000";
 export const USER_UPLOAD_PATH = "uploads/users";
 export const COURSE_UPLOAD_PATH = "uploads/courses/images";
 export const VALID_STATUS = ["DRAFT", "PUBLISHED"] as const;

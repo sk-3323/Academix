cd12d66d7b45caf15ec9f78a7e4550c0743c8ebf
RISHI GAIWALA <khushilr.msquaretechonologies@gmail.com>
2025-03-08 00:48:08 +0530

    repush

--- src/app/(main)/pricing/page.tsx
+++ src/app/(main)/pricing/page.tsx
@@ -2,144 +2,119 @@ import React from "react";
 import { Gem, CheckCircle, XCircle, Crown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-const pricingPlans = [
-  {
-    id: 1,
-    title: "Basic",
-    price: "FREE",
-    icon: Gem,
-    features: [
-      "Conference plans",
-      "Free Lunch And Coffee",
-      "Certificate",
-      "Easy Access",
-      "Free Contacts",
-    ],
-    available: [true, true, true, false, false],
-    color: "#151515",
-    buttonText: "Current Plan",
-    isDisabled: true,
-  },
-  {
-    id: 2,
-    title: "Premium",
-    price: "₹299",
-    icon: Crown,
-    features: [
-      "Conference plans",
-      "Free Lunch And Coffee",
-      "Certificate",
-      "Easy Access",
-      "Free Contacts",
-    ],
-    available: [true, true, true, true, false],
-    color: "#151515",
-    buttonText: "Upgrade Premium Plan",
-    isDisabled: false,
-  },
-];
-
-const page = () => {
+const Page = () => {
   return (
-    <div>
-      <div className="min-h-screen flex justify-center items-center">
-        <section className="py-16 text-center">
-          <div className="mb-12">
-            <span className="text-lg font-semibold uppercase text-emerald-500">
-              Get plan
-            </span>
-            <h2 className="text-4xl mt-2 mb-8">Choose a Plan</h2>
-          </div>
+    <div className="min-h-screen flex justify-center items-center">
+      <section className="py-16 text-center">
+        <div className="mb-12">
+          <span className="text-lg font-semibold uppercase text-emerald-500">
+            Get plan
+          </span>
+          <h2 className="text-4xl mt-2 mb-8">Choose a Plan</h2>
+        </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 justify-center">
-            {pricingPlans.map((plan) => (
-              <div key={plan.id} className="max-w-md mx-auto">
-                <div
-                  className="relative bg-white dark:bg-black shadow-xl rounded-3xl overflow-hidden"
-                  style={{ borderBottom: `20px solid ${plan.color}` }}
-                >
-                  <div
-                    className="relative pt-12 pb-16 px-8 text-center"
-                    style={{ backgroundColor: plan.color }}
-                  >
-                    <div className="absolute left-0 bottom-0 h-20 w-full bg-white dark:bg-black rounded-t-full"></div>
-                    <div className="relative z-10">
-                      <div className="h-40 w-40 bg-white dark:bg-black rounded-full mx-auto p-2.5">
-                        <div
-                          className="h-full w-full rounded-full flex items-center justify-center"
-                          style={{ border: `5px solid ${plan.color}` }}
-                        >
-                          <plan.icon
-                            size={70}
-                            className="transition-transform duration-500 ease-in-out group-hover:rotate-360"
-                            style={{ color: plan.color }}
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="px-6 py-6">
-                    <div className="text-xl font-semibold text-gray-800">
-                      {plan.title}
-                    </div>
-                    <h4
-                      className="text-2xl font-bold my-2"
-                      style={{ color: plan.color }}
-                    >
-                      {plan.price}
-                    </h4>
-
-                    <ul className="my-6 mx-4 space-y-2">
-                      {plan.features.map((feature, index) => (
-                        <li
-                          key={index}
-                          className={`flex items-center justify-start py-2 border-b border-dashed border-gray-200 text-gray-600`}
-                        >
-                          {plan.available[index] ? (
-                            <CheckCircle
-                              size={18}
-                              className="mr-3 text-gray-900"
-                            />
-                          ) : (
-                            <XCircle size={18} className="mr-3 text-gray-900" />
-                          )}
-                          <span>{feature}</span>
-                        </li>
-                      ))}
-                    </ul>
-
-                    <div className="mt-8 mb-4">
-                      <Button
-                        disabled={plan.isDisabled}
-                        className={`px-6 py-2 font-bold rounded ${plan.isDisabled ? "cursor-not-allowed" : "cursor-pointer"}`}
-                        style={{
-                          backgroundColor: plan.isDisabled
-                            ? "transparent"
-                            : plan.color,
-                          borderColor: plan.isDisabled
-                            ? "transparent"
-                            : plan.color,
-                          color: plan.isDisabled ? plan.color : "#fff",
-                        }}
-                      >
-                        {plan.buttonText}{" "}
-                        {plan.buttonText.includes("Upgrade") && (
-                          <Crown
-                            size={16}
-                            className="inline-block ml-2 relative -mt-1"
-                          />
-                        )}
-                      </Button>
-                    </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 justify-center">
+          {/* Basic Plan */}
+          <div className="max-w-md mx-auto">
+            <div className="relative bg-white dark:bg-black shadow-xl rounded-3xl overflow-hidden border-b-8 border-gray-900">
+              <div className="relative pt-12 pb-16 px-8 text-center bg-gray-900">
+                <div className="absolute left-0 bottom-0 h-20 w-full bg-white dark:bg-black rounded-t-full"></div>
+                <div className="relative z-10">
+                  <div className="h-40 w-40 bg-white dark:bg-black rounded-full mx-auto p-2.5 border-4 border-gray-900 flex items-center justify-center">
+                    <Gem size={70} className="text-gray-900" />
                   </div>
                 </div>
               </div>
-            ))}
+              <div className="px-6 py-6 text-center">
+                <div className="text-xl font-semibold text-gray-800">Basic</div>
+                <h4 className="text-2xl font-bold my-2 text-gray-900">FREE</h4>
+                <ul className="my-6 mx-4 space-y-2 text-gray-600">
+                  <li className="flex items-center py-2 border-b border-dashed border-gray-200">
+                    <CheckCircle size={18} className="mr-3 text-gray-900" />{" "}
+                    Conference plans
+                  </li>
+                  <li className="flex items-center py-2 border-b border-dashed border-gray-200">
+                    <CheckCircle size={18} className="mr-3 text-gray-900" />{" "}
+                    Free Lunch And Coffee
+                  </li>
+                  <li className="flex items-center py-2 border-b border-dashed border-gray-200">
+                    <CheckCircle size={18} className="mr-3 text-gray-900" />{" "}
+                    Certificate
+                  </li>
+                  <li className="flex items-center py-2 border-b border-dashed border-gray-200">
+                    <XCircle size={18} className="mr-3 text-gray-900" /> Easy
+                    Access
+                  </li>
+                  <li className="flex items-center py-2 border-b border-dashed border-gray-200">
+                    <XCircle size={18} className="mr-3 text-gray-900" /> Free
+                    Contacts
+                  </li>
+                </ul>
+                <div className="mt-8 mb-4">
+                  <Button
+                    disabled
+                    className="px-6 py-2 font-bold rounded cursor-not-allowed text-gray-900 border border-gray-900 bg-transparent"
+                  >
+                    Current Plan
+                  </Button>
+                </div>
+              </div>
+            </div>
           </div>
-        </section>
-      </div>
+
+          {/* Premium Plan */}
+          <div className="max-w-sm mx-auto">
+            <div className="relative bg-white dark:bg-black shadow-xl rounded-3xl overflow-hidden border-b-8 border-yellow-500">
+              <div className="relative pt-12 pb-16 px-8 text-center bg-yellow-500">
+                <div className="absolute left-0 bottom-0 h-20 w-full bg-white dark:bg-black rounded-t-full"></div>
+                <div className="relative z-10">
+                  <div className="h-40 w-40 bg-white dark:bg-black rounded-full mx-auto p-2.5 border-4 border-yellow-500 flex items-center justify-center">
+                    <Crown size={70} className="text-yellow-500" />
+                  </div>
+                </div>
+              </div>
+              <div className="px-6 py-6 text-center">
+                <div className="text-xl font-semibold text-gray-800">
+                  Premium
+                </div>
+                <h4 className="text-2xl font-bold my-2 text-yellow-500">
+                  ₹299
+                </h4>
+                <ul className="my-6 mx-4 space-y-2 text-gray-600">
+                  <li className="flex items-center py-2 border-b border-dashed border-gray-200">
+                    <CheckCircle size={18} className="mr-3 text-gray-900" />{" "}
+                    Conference plans
+                  </li>
+                  <li className="flex items-center py-2 border-b border-dashed border-gray-200">
+                    <CheckCircle size={18} className="mr-3 text-gray-900" />{" "}
+                    Free Lunch And Coffee
+                  </li>
+                  <li className="flex items-center py-2 border-b border-dashed border-gray-200">
+                    <CheckCircle size={18} className="mr-3 text-gray-900" />{" "}
+                    Certificate
+                  </li>
+                  <li className="flex items-center py-2 border-b border-dashed border-gray-200">
+                    <CheckCircle size={18} className="mr-3 text-gray-900" />{" "}
+                    Easy Access
+                  </li>
+                  <li className="flex items-center py-2 border-b border-dashed border-gray-200">
+                    <XCircle size={18} className="mr-3 text-gray-900" /> Free
+                    Contacts
+                  </li>
+                </ul>
+                <div className="mt-8 mb-4">
+                  <Button className="px-6 py-2 font-bold rounded cursor-pointer text-white bg-yellow-500 border border-yellow-500">
+                    Upgrade Premium Plan{" "}
+                    <Crown size={16} className="inline-block ml-2 -mt-1" />
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
 
-export default page;
+export default Page;

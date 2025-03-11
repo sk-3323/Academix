e99d7d9c3ec5ae798e1964e22822b48dd1db633e
Shubham Kaniya <166137543+sk-3323@users.noreply.github.com>
2025-03-11 11:29:32 +0530

    Update Node app url
    Signed-off-by: Shubham Kaniya <166137543+sk-3323@users.noreply.github.com>

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,8 @@
-export const NODE_APP_URL = "http://localhost:3000";
+//Production
+export const NODE_APP_URL = "https://academix-learning.netlify.app";
+
+// Local
+// export const NODE_APP_URL = "http://localhost:3000";
 export const USER_UPLOAD_PATH = "uploads/users";
 export const COURSE_UPLOAD_PATH = "uploads/courses/images";
 export const VALID_STATUS = ["DRAFT", "PUBLISHED"] as const;

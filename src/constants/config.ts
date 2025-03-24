ea94803a8e930953cb52bffd400a6a825fa37689
RISHI GAIWALA <khushilr.msquaretechonologies@gmail.com>
2025-03-25 00:13:07 +0530

    url change

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,6 @@
 //Production
-// export const NODE_APP_URL = "https://academix-learning.netlify.app";
-export const NODE_APP_URL = "http://localhost:3000";
+export const NODE_APP_URL = "https://academix-learning.netlify.app";
+// export const NODE_APP_URL = "http://localhost:3000";
 // export const NODE_APP_URL =
 //   process.env.NODE_ENV === "production"
 //     ? "https://academix-learning.netlify.app"
